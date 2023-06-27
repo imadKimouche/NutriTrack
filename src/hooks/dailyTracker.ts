618d83cdc664297078cbdeb8fd0aed3ed052762f
imadKimouche <imad.kimouche@epitech.eu>
2023-06-27 22:12:54 +0200

    [fix] formatted dates are now refreshed when current selected date is refreshed

--- src/hooks/dailyTracker.ts
+++ src/hooks/dailyTracker.ts
@@ -1,17 +1,34 @@
-import {useState} from 'react';
-
-const TODAY = new Date();
+import {useEffect, useState} from 'react';
 
 export type MealType = 'breakfast' | 'lunch' | 'dinner' | 'snack';
 
 export function useCurrentMealData() {
-  const [currentSelectedDate, setCurrentSelectedDate] = useState(TODAY.getTime());
+  const [currentSelectedDate, setCurrentSelectedDate] = useState<number>(0);
   const [currentMealType, setCurrentMealType] = useState<MealType>('breakfast');
+  const [stringFormattedDate, setStringFormattedDate] = useState('');
+
+  useEffect(() => {
+    const TODAY = new Date();
+    setCurrentSelectedDate(TODAY.getTime());
+    setStringFormattedDate(formatDate(TODAY));
+  }, []);
+
+  useEffect(() => {
+    const selectedDate = new Date(currentSelectedDate);
+    setStringFormattedDate(formatDate(selectedDate));
+  }, [currentSelectedDate]);
+
+  const formatDate = (date: Date): string => {
+    const day = date.getDate();
+    const month = date.getMonth() + 1;
+    const year = date.getFullYear();
+    return `${day}-${month}-${year}`;
+  };
 
   return {
     currentSelectedDate,
     setCurrentSelectedDate,
-    stringFormattedDate: `${TODAY.getDate()}-${TODAY.getMonth()}-${TODAY.getFullYear()}`,
+    stringFormattedDate,
     currentMealType,
     setCurrentMealType,
   };

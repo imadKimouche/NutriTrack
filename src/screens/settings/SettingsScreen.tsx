00bd04d624404f2916e892e5d7edca0c1afca384
imadKimouche <imad.kimouche@epitech.eu>
2023-07-07 23:51:40 +0200

    [wip] user settings screen

--- src/screens/settings/SettingsScreen.tsx
+++ src/screens/settings/SettingsScreen.tsx
@@ -1,29 +1,26 @@
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+import {ResponsiveValue} from '@shopify/restyle';
 import React from 'react';
 import Box from '../../atoms/Box';
 import Pressable from '../../atoms/Pressable';
 import Text from '../../atoms/Text';
 import Icon from '../../components/Icon';
 import {signOut} from '../../hooks/auth';
+import {Theme} from '../../style/theme';
 import {HomeStackParamList} from '../HomeStackNavigator';
 
 type SettingsItemProps = {
   label: string;
   icon: string;
   onPress: () => void;
+  bg: ResponsiveValue<keyof Theme['colors'], Theme['breakpoints']>;
+  c: keyof Theme['colors'];
 };
-const SettingsItem: React.FC<SettingsItemProps> = ({label, icon, onPress}) => {
+const SettingsItem: React.FC<SettingsItemProps> = ({label, icon, onPress, bg, c}) => {
   return (
     <Pressable flexDirection={'row'} alignItems={'center'} onPress={onPress} p={'s'}>
-      <Box
-        width={42}
-        height={42}
-        marginRight={'m'}
-        borderRadius={'md'}
-        bg={'$logoutIconBackground'}
-        alignItems={'center'}
-        justifyContent={'center'}>
-        <Icon name={icon} size={18} color={'$logoutIcon'} />
+      <Box width={42} height={42} marginRight={'m'} borderRadius={'md'} bg={bg} alignItems={'center'} justifyContent={'center'}>
+        <Icon name={icon} size={18} color={c} />
       </Box>
       <Text variant={'subtitle1'}>{label}</Text>
     </Pressable>
@@ -34,9 +31,21 @@ type SettingsScreenNavigationProp = NativeStackNavigationProp<HomeStackParamList
 const SettingsScreen: React.FC<{navigation: SettingsScreenNavigationProp}> = ({navigation}) => {
   return (
     <Box flex={1} bg={'$background'} p={'s'}>
-      <SettingsItem label="Profil" icon="user" onPress={() => navigation.navigate('ProfileSettings')} />
-      <SettingsItem label="Fitness" icon="chevrons-up" onPress={() => navigation.navigate('FitnessSettings')} />
-      <SettingsItem label="Se déconnecter" icon="log-out" onPress={signOut} />
+      <SettingsItem
+        label="Profil"
+        bg={'$modalShadowDrop'}
+        c={'$primary'}
+        icon="user"
+        onPress={() => navigation.navigate('ProfileSettings')}
+      />
+      <SettingsItem
+        label="Fitness"
+        bg={'$modalShadowDrop'}
+        c={'$primary'}
+        icon="chevrons-up"
+        onPress={() => navigation.navigate('FitnessSettings')}
+      />
+      <SettingsItem label="Se déconnecter" bg={'$logoutIconBackground'} c={'$logoutIcon'} icon="log-out" onPress={signOut} />
     </Box>
   );
 };

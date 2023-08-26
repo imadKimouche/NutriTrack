4328da9f9a8d9b56f0c48bd9195fcef25142520e
imadKimouche <imad.kimouche@epitech.eu>
2023-08-26 15:41:53 +0200

    [fix] aboutYou screen ui

--- src/screens/onboarding/AboutYouTab.tsx
+++ src/screens/onboarding/AboutYouTab.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {ReactNode, useEffect, useMemo, useRef, useState} from 'react';
 import Box from '../../atoms/Box';
 import Text from '../../atoms/Text';
 import Button from '../../components/Button';
@@ -11,7 +11,12 @@ import Picker from '../../components/Picker';
 import TextInput from '../../components/TextInput';
 import {KeyboardAvoidingView, Platform} from 'react-native';
 import ListItem from '../../components/ListItem';
+import BottomSheet from '@gorhom/bottom-sheet';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import {useTheme} from '@shopify/restyle';
+import {Theme} from '../../style/theme';
 
+// TODO useMemo
 export const HEIGHT_OPTIONS = generateHeightOptions(120, 230, 1);
 export const WEIGHT_OPTIONS = generateWeightOptions(30, 180, 1);
 
@@ -22,8 +27,21 @@ export const GENDERS: GenderItem[] = [
   {id: 'female', label: 'Femme', icon: 'user'},
 ];
 
+const Section: React.FC<{title: string; children: ReactNode}> = ({title, children}) => {
+  return (
+    <Box mb={'l'}>
+      <Text variant={'text-medium'} color={'$primary'}>
+        {title}
+      </Text>
+      <>{children}</>
+    </Box>
+  );
+};
+
 export type AboutYouTabProps = {navigation: TabNavigationProp};
 const AboutYouTab: React.FC<AboutYouTabProps> = ({navigation}) => {
+  const insets = useSafeAreaInsets();
+  const {spacing} = useTheme<Theme>();
   const {gender, setGender, setAge, age, height, setHeight, weight, setWeight} = useOnBoardingStore(state => ({
     gender: state.gender,
     setGender: state.setGender,
@@ -37,12 +55,25 @@ const AboutYouTab: React.FC<AboutYouTabProps> = ({navigation}) => {
   const [ageStr, setAgeStr] = useState('');
   const canAdvance = gender !== undefined && age !== undefined;
 
+  const [bottomSheetType, setBottomSheetType] = useState<'height' | 'weight' | undefined>(undefined);
+  const bottomSheetRef = useRef<BottomSheet>(null);
+  const snapPoints = useMemo(() => ['30%'], []);
+
   useEffect(() => {
     const numValue = parseInt(ageStr, 10);
     if (!isNaN(numValue)) {
       setAge(numValue);
     }
   }, [ageStr, setAge]);
+
+  function openBottomSheetFor(type: 'height' | 'weight' | undefined) {
+    if (type === undefined) {
+      bottomSheetRef.current?.close();
+    } else {
+      setBottomSheetType(type);
+      bottomSheetRef.current?.expand();
+    }
+  }
 
   return (
     <Box flex={1} bg={'$bgWeak'}>
@@ -51,10 +82,7 @@ const AboutYouTab: React.FC<AboutYouTabProps> = ({navigation}) => {
         leftComponent={<GoBackButton onPress={() => navigation.navigate('activityLevel')} />}
       />
       <Box flex={1} p={'m'}>
-        <Box mb={'m'}>
-          <Text variant={'text-small'} color={'$header'}>
-            Quel est votre sexe ?
-          </Text>
+        <Section title="Genre">
           {GENDERS.map(item => (
             <ListItem
               key={item.id}
@@ -63,18 +91,9 @@ const AboutYouTab: React.FC<AboutYouTabProps> = ({navigation}) => {
               title={item.label}
             />
           ))}
-        </Box>
-        <Box mb={'m'} flexDirection={'row'} alignItems={'center'} justifyContent={'space-between'}>
-          <Text variant={'text-small'} color={'$header'}>
-            Age
-          </Text>
-          <KeyboardAvoidingView
-            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
-            style={{
-              flex: 0.55,
-              alignSelf: 'stretch',
-              justifyContent: 'center',
-            }}>
+        </Section>
+        <Section title="Âge">
+          <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={{marginTop: spacing.m}}>
             <TextInput
               alignSelf={'stretch'}
               placeholder="Entrez votre age"
@@ -83,33 +102,31 @@ const AboutYouTab: React.FC<AboutYouTabProps> = ({navigation}) => {
               inputPropPresets={'positiveNumber'}
             />
           </KeyboardAvoidingView>
-        </Box>
-        <Box mb={'m'} flexDirection={'row'} alignItems={'center'} justifyContent={'space-between'}>
-          <Text variant={'text-small'} color={'$header'}>
-            Taille
-          </Text>
-          <Picker
-            containerStyle={{flex: 0.6}}
-            selectedValue={height}
-            onValueChange={itemValue => setHeight(itemValue as typeof height)}>
-            {HEIGHT_OPTIONS.map(option => {
-              return <Picker.Item key={option.label} label={option.label} value={option.value} />;
-            })}
-          </Picker>
-        </Box>
-        <Box flexDirection={'row'} alignItems={'center'} justifyContent={'space-between'}>
-          <Text variant={'text-small'} color={'$header'}>
-            Poids
-          </Text>
-          <Picker
-            containerStyle={{flex: 0.6}}
-            selectedValue={weight}
-            onValueChange={itemValue => setWeight(itemValue as typeof weight)}>
-            {WEIGHT_OPTIONS.map(option => {
-              return <Picker.Item key={option.label} label={option.label} value={option.value} />;
-            })}
-          </Picker>
-        </Box>
+        </Section>
+        <Section title="Taille & Poids">
+          <ListItem
+            title="Taille"
+            onPress={() => openBottomSheetFor('height')}
+            variant={bottomSheetType === 'height' ? 'active' : undefined}
+            rightComponent={
+              <Text variant={'text-small-tight'} color={'$label'}>
+                {height} cm
+              </Text>
+            }
+            rightComponentProps={{flex: 0.5, alignItems: 'flex-end'}}
+          />
+          <ListItem
+            title="Poids"
+            onPress={() => openBottomSheetFor('weight')}
+            variant={bottomSheetType === 'weight' ? 'active' : undefined}
+            rightComponent={
+              <Text variant={'text-small-tight'} color={'$label'}>
+                {weight} kg
+              </Text>
+            }
+            rightComponentProps={{flex: 0.5, alignItems: 'flex-end'}}
+          />
+        </Section>
       </Box>
       <Box flex={0.5} justifyContent={'center'} alignItems={'center'} px={'xl'}>
         <Button
@@ -120,6 +137,46 @@ const AboutYouTab: React.FC<AboutYouTabProps> = ({navigation}) => {
           icon="chevron-right"
         />
       </Box>
+      <BottomSheet
+        keyboardBehavior="interactive"
+        ref={bottomSheetRef}
+        index={-1}
+        enablePanDownToClose={true}
+        onClose={() => setBottomSheetType(undefined)}
+        snapPoints={snapPoints}
+        bottomInset={insets.bottom}>
+        {bottomSheetType === 'height' && (
+          <Box px={'m'}>
+            <Text py={'xs'} variant={'text-medium'}>
+              Renseignez votre taille
+            </Text>
+            <Picker
+              itemStyle={{height: '90%'}}
+              selectedValue={height}
+              onValueChange={itemValue => setHeight(itemValue as typeof height)}>
+              {HEIGHT_OPTIONS.map(option => {
+                return <Picker.Item key={option.label} label={option.label} value={option.value} />;
+              })}
+            </Picker>
+          </Box>
+        )}
+
+        {bottomSheetType === 'weight' && (
+          <Box px={'m'}>
+            <Text py={'xs'} variant={'text-medium'}>
+              Renseignez votre poids
+            </Text>
+            <Picker
+              itemStyle={{height: '90%'}}
+              selectedValue={weight}
+              onValueChange={itemValue => setWeight(itemValue as typeof weight)}>
+              {WEIGHT_OPTIONS.map(option => {
+                return <Picker.Item key={option.label} label={option.label} value={option.value} />;
+              })}
+            </Picker>
+          </Box>
+        )}
+      </BottomSheet>
     </Box>
   );
 };

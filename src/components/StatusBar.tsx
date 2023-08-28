6e703046fe61269b5bf1f14896a64ab6b5e30202
imadKimouche <imad.kimouche@epitech.eu>
2023-08-28 18:54:15 +0200

    [fix] statusbar bg

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -3,19 +3,21 @@ import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import Box from '../atoms/Box';
 import {Theme} from '../style/theme';
 import {StatusBar as NativeStatusBar} from 'react-native';
+import {useTheme} from '@shopify/restyle';
 
 const StatusBar: React.FC<{backgroundColor?: keyof Theme['colors']; style?: 'light' | 'dark'}> = ({
-  backgroundColor = '$navbarBg',
+  backgroundColor,
   style = 'dark',
 }) => {
   const insets = useSafeAreaInsets();
+  const {colors} = useTheme<Theme>();
 
   return (
-    <Box style={{height: insets.top, backgroundColor: backgroundColor}}>
+    <Box bg={backgroundColor ?? '$bgWeak'} style={{height: insets.top}}>
       <NativeStatusBar
         barStyle={style === 'dark' ? 'dark-content' : 'light-content'}
         animated={true}
-        backgroundColor={backgroundColor}
+        backgroundColor={colors[backgroundColor ?? '$bgWeak']}
       />
     </Box>
   );
